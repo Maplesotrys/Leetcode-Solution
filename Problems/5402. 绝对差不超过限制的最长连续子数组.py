b5452778068de95b61f6abb10028a16c57020a7f
Maplestorys <45001578+Maplesotrys@users.noreply.github.com>
2020-05-03 14:13:19 +0800

    Update 5402. 绝对差不超过限制的最长连续子数组.py
    滑动窗口，维护窗口内的最大最小值

--- Problems/5402. 绝对差不超过限制的最长连续子数组.py
+++ Problems/5402. 绝对差不超过限制的最长连续子数组.py
@@ -1,3 +1,4 @@
+#滑动窗口，维护窗口内的最大最小值
 def longestSubarray2(nums, limit):
     left = 0
     right = 0
@@ -10,6 +11,7 @@ def longestSubarray2(nums, limit):
         if max_ - min_ <=limit:
             res = max(res, right - left + 1)
         right += 1
+        #当最大值减最小值大于限制条件时，且窗口不为空时，左边界 + 1
         while max_ - min_ > limit and left < right:
             left += 1
             max_ = max(nums[left:right + 1])
