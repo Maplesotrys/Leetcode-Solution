0d10304321d3163c65570cd6844feae10de295b9
Maplestorys <45001578+Maplesotrys@users.noreply.github.com>
2020-05-03 13:46:54 +0800

    Update 5402. 绝对差不超过限制的最长连续子数组.py

--- Problems/5402. 绝对差不超过限制的最长连续子数组.py
+++ Problems/5402. 绝对差不超过限制的最长连续子数组.py
@@ -1,17 +1,17 @@
 def longestSubarray2(nums, limit):
-        left = 0
-        right = 0
-        res = 0
-        max_ = 0
-        min_ = 0
-        while right < len(nums):
-            max_ = max(nums[left:right+1])
-            min_ = min(nums[left:right+1])
-            if max_ - min_ <=limit:
-                res = max(res, right - left + 1)
-            right += 1
-            while max_ - min_ > limit and left < right:
-                left += 1
-                max_ = max(nums[left:right + 1])
-                min_ = min(nums[left:right + 1])    
-        return res
+    left = 0
+    right = 0
+    res = 0
+    max_ = 0
+    min_ = 0
+    while right < len(nums):
+        max_ = max(nums[left:right+1])
+        min_ = min(nums[left:right+1])
+        if max_ - min_ <=limit:
+            res = max(res, right - left + 1)
+        right += 1
+        while max_ - min_ > limit and left < right:
+            left += 1
+            max_ = max(nums[left:right + 1])
+            min_ = min(nums[left:right + 1])    
+    return res
